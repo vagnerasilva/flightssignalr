f5645d6489ad542e56649452717162bc4b6bab73
vagner <vagnerantoniodasilva@yahoo.com.br>
2018-08-24 16:30:29 -0300

    Add files via upload

---
+++ OnDocumentsChanged/index.js
@@ -0,0 +1,10 @@
+module.exports = function (context, updatedFlights) {
+  context.bindings.signalRMessages =
+
+    updatedFlights.map(flight => ({
+      target: 'flightUpdated',
+      arguments: [flight]
+    }));
+    
+  context.done();
+};
